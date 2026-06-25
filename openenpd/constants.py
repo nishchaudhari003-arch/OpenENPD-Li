59776e10f3cdc8d371fa2bf9a4574252e46acedd
nishchaudhari003-arch <nishchaudhari003@gmail.com>
2026-06-25 14:22:39 -0400

    Add Avogadro constant

--- openenpd/constants.py
+++ openenpd/constants.py
@@ -5,3 +5,4 @@ Physical constants used in OpenENPD-Li.
 R_GAS = 8.314462618  # J mol^-1 K^-1
 FARADAY = 96485.33212  # C mol^-1
 EPSILON_0 = 8.8541878128e-12  # F m^-1
+AVOGADRO = 6.02214076e23  # mol^-1
